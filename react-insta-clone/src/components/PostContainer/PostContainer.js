9a5101085169b76d0ec747eb036bea481ced2f93
jdspell <spelljd15@gmail.com>
2019-02-19 10:34:57 -0500

    added some styling to the post components

--- react-insta-clone/src/components/PostContainer/PostContainer.js
+++ react-insta-clone/src/components/PostContainer/PostContainer.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import "./PostContainer.scss";
 import CommentsSection from '../CommentsSection/CommentsSection';
 
 const PostContainer = props => {
     return(
         <div className="post">
-            <p><img src={props.post.thumbnailUrl} /> <strong>{props.post.username}</strong></p>
+            <div className="postHeading">
+                <img src={props.post.thumbnailUrl} /> 
+                <strong> {props.post.username}</strong>
+            </div>
+
             <img src={props.post.imageUrl} />
+
             <div className="icons">
                 <img src={require("../../Images/searchBarHeart.svg")} />
                 <img src={require("../../Images/postSpeechBubble.svg")} />
             </div>
-            <p><strong>{props.post.likes} likes</strong></p>
+
+            <div className="likes"><strong>{props.post.likes} likes</strong></div>
+
             <CommentsSection comments={props.post.comments} />
+
             <input type="text" placeholder="Add a comment..." />
         </div>
     );
